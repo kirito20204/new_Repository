5b737b8c70a638053a01d8ccb4e3e7d3688d9db7
Sobolev515 <alex.sobolev515@yandex.ru>
2022-05-26 19:37:06 +0500

    Fix

--- Совместнная работа.py
+++ Совместнная работа.py
@@ -1,4 +1,2 @@
-a = input('Введите время в мин. и растояние в км.').split()
-b = int(a[0])*60
-c = int(a[1])*1000
-print(c/b)
+time, distance = map(int, input('Введите время в мин. и растояние в км.').split())
+print((distance*1000)/(time*60))

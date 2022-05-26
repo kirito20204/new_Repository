474e2bcb66a006d2cb1a0b46e3ad886c62c2d53c
kirito20204 <105800566+kirito20204@users.noreply.github.com>
2022-05-26 19:40:49 +0500

    push new_file

--- Совместнная работа.py
+++ Совместнная работа.py
@@ -1,2 +1,4 @@
-time, distance = map(int, input('Введите время в мин. и растояние в км.').split())
-print((distance*1000)/(time*60))
+task = input('Введите время в мин. и растояние в км.').split()
+time = int(task[0])*60
+distance = int(task[1])*1000
+print(distance/time)
